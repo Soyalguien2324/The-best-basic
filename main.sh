51535ac7b07d1ce24738fde6c80d583b11478238
Soyalguien2324 <131987116+Soyalguien2324@users.noreply.github.com>
2023-10-23 20:11:27 +0200

    update main.sh

--- main.sh
+++ main.sh
@@ -1,7 +1,94 @@
 #!/bin/bash
 sudo apt update -y &>/dev/null
 sudo apt upgrade -y &>/dev/null
-sudo apt install -y zsh git curl wget fzf php gcc apache2 neofetch docker docker-compose docker.io &>/dev/null
+sudo apt install -y zsh git micro curl wget fzf php gcc apache2 neofetch docker docker-compose docker.io &>/dev/null
 sh -c 'git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /usr/share/powerlevel10k' &>/dev/null
-sh -c 'echo 'source ~/powerlevel10k/powerlevel10k.zsh-theme' >>~/.zshrc' &>/dev/null
-exit 0 || exit 1
+cat <<EOL>> ~/.zshrc
+export _JAVA_AWT_WM_NONREPARENTING=1
+if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
+source "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh"
+fi
+autoload -Uz promptinit
+promptinit
+prompt adam1
+setopt histignorealldups sharehistory
+bindkey -e
+HISTSIZE=1000
+SAVEHIST=1000
+HISTFILE=~/.zsh_history
+autoload -Uz compinit
+compinit
+zstyle ':completion:*' auto-description 'specify: %d'
+zstyle ':completion:*' completer _expand _complete _correct _approximate
+zstyle ':completion:*' format 'Completando %d'
+zstyle ':completion:*' group-name ''
+zstyle ':completion:*' menu select=2
+eval "$(dircolors -b)"
+zstyle ':completion:*:default' list-colors ${(s.:.)LS_COLORS}
+zstyle ':completion:*' list-colors ''
+zstyle ':completion:*' list-prompt %SAt %p: Hit TAB for more, or the character to insert%s
+zstyle ':completion:*' matcher-list '' 'm:{a-z}={A-Z}' 'm:{a-zA-Z}={A-Za-z}' 'r:|[._-]=* r:|=* l:|=*'
+zstyle ':completion:*' menu select=long
+zstyle ':completion:*' select-prompt %SScrolling active: current selection at %p%s
+zstyle ':completion:*' use-compctl false
+zstyle ':completion:*' verbose true
+zstyle ':completion:*:*:kill:*:processes' list-colors '=(#b) #([0-9]#)*=0=01;31'
+zstyle ':completion:*:kill:*' command 'ps -u $USER -o pid,%cpu,tty,cputime,cmd'
+source /usr/share/powerlevel10k/powerlevel10k.zsh-theme
+[[ -f ~/.p10k.zsh ]] && source ~/.p10k.zsh
+PATH=/root/.local/bin:/snap/bin:/usr/sandbox/:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games:/usr/share/games:/usr/local/sbin:/usr/sbin:/sbin:/usr/local/bin:/usr/bin:/bin:/usr/local/games:/usr/games
+function iokls {
+	scrub -p dod $1
+	shred -zun 10 -v $1
+}
+alias rm='iokls'
+alias ll='ls -l'
+alias la='ls -a'
+alias l='ls'
+alias lla='ls -la'
+alias cat='batcat'
+alias dir='ls'
+alias vim='micro'
+alias vi='micro'
+[ -f ~/.fzf.zsh ] && . ~/.fzf.zsh
+. /usr/share/zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+. /usr/share/zsh-autosuggestions/zsh-autosuggestions.zsh
+. /usr/share/zsh-sudo/sudo.plugin.zsh
+function man() {
+    env \
+    LESS_TERMCAP_mb=$'\e[01;31m' \
+    LESS_TERMCAP_md=$'\e[01;31m' \
+    LESS_TERMCAP_me=$'\e[0m' \
+    LESS_TERMCAP_se=$'\e[0m' \
+    LESS_TERMCAP_so=$'\e[01;44;33m' \
+    LESS_TERMCAP_ue=$'\e[0m' \
+    LESS_TERMCAP_us=$'\e[01;32m' \
+    man "$@"
+}
+function fzf-lovely {
+	if [ "$1" = "h" ]; then
+		fzf -m --reverse --preview-window down:20 --preview '[[ $(file --mime {}) =~ binary ]] &&
+ 	                echo {} is a binary file ||
+	                 (bat --style=numbers --color=always {} ||
+	                  highlight -O ansi -l {} ||
+	                  coderay {} ||
+	                  rougify {} ||
+	                  cat {}) 2> /dev/null | head -500'
+	else
+	        fzf -m --preview '[[ $(file --mime {}) =~ binary ]] &&
+	                         echo {} is a binary file ||
+	                         (bat --style=numbers --color=always {} ||
+	                          highlight -O ansi -l {} ||
+	                          coderay {} ||
+	                          rougify {} ||
+	                          cat {}) 2> /dev/null | head -500'
+	fi
+}
+
+(( ! ${+functions[p10k-instant-prompt-finalize]} )) || p10k-instant-prompt-finalize
+EOL
+sudo usermod root --shell /bin/zsh
+export pokl=$USER
+sudo usermod $pokl --shell /bin/zsh
+sudo ln -s /home/$pokl/.zshrc /root/.zshrc
+exit 0 || exit 1   
