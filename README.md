a91555cd920d1bfbb97202abbb64f30b1bcf8c89
Soyalguien2324 <131987116+Soyalguien2324@users.noreply.github.com>
2023-10-23 20:20:39 +0200

    Update README & main.sh

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 # The-best-basic
+
+To install type this commands:
+```bash
+git clone https://github.com/Soyalguien2324/The-best-basic.git
+cd The-best-basic
+chmod a+x main.sh
+./main.sh
+```
--- main.sh
+++ main.sh
@@ -2,7 +2,7 @@
 sudo apt update -y &>/dev/null
 sudo apt upgrade -y &>/dev/null
 sudo apt install -y zsh git micro curl wget fzf php gcc apache2 neofetch docker docker-compose docker.io &>/dev/null
-sh -c 'git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /usr/share/powerlevel10k' &>/dev/null
+sh -c 'sudo git clone --depth=1 https://github.com/romkatv/powerlevel10k.git /usr/share/powerlevel10k' &>/dev/null
 cat <<EOL>> ~/.zshrc
 export _JAVA_AWT_WM_NONREPARENTING=1
 if [[ -r "${XDG_CACHE_HOME:-$HOME/.cache}/p10k-instant-prompt-${(%):-%n}.zsh" ]]; then
